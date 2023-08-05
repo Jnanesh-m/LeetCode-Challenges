9d06347cac3074b97e27b4b39da5d0946806af54
Jnanesh M <128501137+Jnanesh-m@users.noreply.github.com>
2023-08-05 22:31:55 +0530

    Palindrome Number.java
    9. Palindrome Number
    
    Difficulty Level: Easy
    
    Problem Statement: Given an integer x, return true if x is a 
    palindrome, and false otherwise.

---
+++ Palindrome Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+       if(x<0 || x!=0 && x%10==0) 
+       return false; 
+       int c=0;
+       while(x>c)
+       {
+            c=c*10 + x%10;
+            x/=10;
+       }
+       return(x==c || x==c/10);
+    }
+}
